f75b6ed4573184ac6ae0ef06db9ab8e1ebaf9d26
Dmitry <d@dmitriy-pro.local>
2017-07-15 19:35:36 +0300

    Initial Commit

---
+++ P5Sandbox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  P5Sandbox
+//
+//  Created by Dmitry on 15/07/2017.
+//  Copyright © 2017 Dmitry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ P5Sandbox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  P5Sandbox
+//
+//  Created by Dmitry on 15/07/2017.
+//  Copyright © 2017 Dmitry. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
